24cd075cc5cfa20822b6ae6929492cc697f174a9
hatoo <hato2000@gmail.com>
2025-06-08 21:26:25 +0900

    a

---
+++ kernels/simple.cu
@@ -0,0 +1,22 @@
+__global__ void simple_matmul_kernel(float *a, float *b, float *c, int n, int m,
+                                     int k) {
+  int row = blockIdx.x * blockDim.x + threadIdx.x;
+  int col = blockIdx.y * blockDim.y + threadIdx.y;
+
+  if (row < m && col < n) {
+    float value = 0.0f;
+    for (int i = 0; i < k; ++i) {
+      value += a[row * k + i] * b[i * n + col];
+    }
+    c[row * n + col] = value;
+  }
+}
+
+void simple(uintptr_t a, uintptr_t b, uintptr_t c, int n, int m, int k) {
+  dim3 blockSize(16, 16);
+  dim3 gridSize((m + blockSize.x - 1) / blockSize.x,
+                (n + blockSize.y - 1) / blockSize.y);
+
+  simple_matmul_kernel<<<gridSize, blockSize>>>((float *)a, (float *)b,
+                                                (float *)c, n, m, k);
+}
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import torch
+from torch.utils.cpp_extension import load_inline
 import time
 
 from collections.abc import Callable
+
+with open('kernels/simple.cu', 'r') as f:
+    simple_src = f.read()
+
+simple = load_inline(
+    name='simple',
+    cpp_sources='void simple(uintptr_t a, uintptr_t b, uintptr_t c, int n, int m, int k);',
+    cuda_sources=simple_src,
+    functions='simple',
+    with_cuda=True,
+    extra_cuda_cflags=['-O3'],
+)
+
+def launch_simple(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor):
+    n, k = a.shape
+    _k, m = b.shape
+    
+    # Call the CUDA kernel
+    simple.simple(a.data_ptr(), b.data_ptr(), c.data_ptr(), n, m, k)
+
 
 def benchmark(
     n: int,
     m: int,
     k: int,
     kernel: Callable[[torch.Tensor, torch.Tensor, torch.Tensor], None]):
-    a = torch.randn(n, m, device='cuda')
-    b = torch.randn(m, k, device='cuda')
-    c = torch.zeros(n, k, device='cuda')
+    a = torch.randn(n, k, dtype=torch.float32, device='cuda')
+    b = torch.randn(k, m, dtype=torch.float32, device='cuda')
+    c = torch.zeros(n, m, dtype=torch.float32, device='cuda')
 
     warmup_times = 4
     times = 8
@@ -19,6 +40,7 @@ def benchmark(
     for _ in range(warmup_times):
         torch.cuda.synchronize()
         kernel(a, b, c)
+        torch.cuda.synchronize()
 
     for _ in range(times):
         torch.cuda.synchronize()
@@ -40,6 +62,7 @@ def main():
 
     # Benchmark the kernel
     benchmark(n, m, k, kernel)
+    benchmark(n, m, k, launch_simple)
 
 
 if __name__ == "__main__":
